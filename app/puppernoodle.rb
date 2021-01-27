4d60e586c853ea4e94420c4cf3de631a89c9e815
Jeffrey Platt <JSPlatt@gmail.com>
2021-01-27 17:07:03 -0500

    pre-reload

--- app/puppernoodle.rb
+++ app/puppernoodle.rb
@@ -13,7 +13,7 @@ class PupperNoodles
   end
 
   def welcome
-    puts "Welcome to PupperNoodles! The app you never you knew you needed!"
+    puts "Welcome to PupperNoodles! The App you never you knew you needed!"
     sleep(1)
     puts "Time to get your playdate started...."
   end
@@ -63,32 +63,39 @@ end
   def choose_a_dog
     puts "Choose the number of the dog you'd like to play with!"
     answer = STDIN.gets.chomp
-    @dog = Dog.all.find(answer).name
+    @dog = Dog.all.find(answer)
     sleep(1)
   end
 
   def make_a_playdate
-    puts "You have selected #{@dog}, would you like to make a playdate? yes/no?".colorize(:green)
+    puts "You have selected #{@dog.name}, would you like to make a playdate? yes/no?".colorize(:green)
     answer = STDIN.gets.chomp
       if answer == "yes"
-         playdate = Playdate.create[answer]
+         playdate = Playdate.create(dog_id: @dog.id, user_id: @user.id, time: Time.now)
         puts "Great! Your playdate is scheduled!"
         sleep(1)
-        puts "Your playdate with #{@dog} is scheduled for January 30th, 2021 at 11:30am! Have fun!".colorize(:green)
+        puts "Your playdate with #{@dog.name} is scheduled for January 30th, 2021 at 11:30am! Have fun!".colorize(:green)
         sleep(2.5)
       else
         puts "Maybe next time..."
       end
     end    
 
+    def all_playdates
+      @user.reload
+    end
+
   def user_cancel
     puts "All done? Would you like to cancel your account? yes/no?"
     answer = STDIN.gets.chomp.downcase
     if answer == "yes"
-      @user = User.last.destroy
+      @user = @user.destroy
       sleep(1)
       puts "Alright, #{@user.username.capitalize}, your username has been deleted"
     else
       puts "Great, we'll see you next time!"
     end
   end
+
+  #use instance methods to ability. Iterate through playdates for a user to tell the name of the dog and ID of the playdate
+  #printing out ID for playdate and Dog
