66554614d15cd90d6a2cc4ce4e1a910d77221693
Jeffrey Platt <JSPlatt@gmail.com>
2021-01-27 12:19:59 -0500

    MVP complete

--- app/puppernoodle.rb
+++ app/puppernoodle.rb
@@ -9,6 +9,7 @@ class PupperNoodles
     change_user_name
     see_all_dogs
     choose_a_dog
+    user_cancel
   end
 
   def welcome
@@ -37,7 +38,13 @@ end
 
   def see_all_dogs
     puts "Let's take a look at those dogs..."
-    sleep(1)
+    sleep(2)
+    puts ".................................."
+    sleep(2)
+    puts "..........MATCHING................"
+    sleep(2)
+    puts ".................................."
+    sleep(3)
     Dog.all.each do |dog_ins|
       puts dog_ins.id_and_name()
     end
@@ -48,10 +55,22 @@ end
     answer = STDIN.gets.chomp
     @dog = Dog.all.find(answer).name
     sleep(1)
-    puts "Your Playdate with #{@dog} is scheduled for "
-    
+    puts "Your Playdate with #{@dog} is scheduled for January 30th, 2021 at 11:30am! Have fun!"
+    sleep(2.5)
   end
-# # Need to figure out how to get the ability to choose dog by id number
+
+  def user_cancel
+    puts "All done? Would you like to cancel your account? yes/no?"
+    answer = STDIN.gets.chomp.downcase
+    if answer == "yes"
+      @user = User.last.destroy
+      sleep(1)
+      puts "Alright, #{@user.username.capitalize}, your username has been deleted"
+    else
+      puts "Great, we'll see you next time!"
+    end
+  end
+
 
     
 
